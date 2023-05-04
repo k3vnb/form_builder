ce615dcfa0d87c34d59369078c480974c83d2a51
Kevin B <papershack@gmail.com>
2023-05-03 22:32:02 -0700

    update readOnly and disabled state ui, update Storybook config

--- src/components/Inputs/TextInputField/bin/TextInputCore.tsx
+++ src/components/Inputs/TextInputField/bin/TextInputCore.tsx
@@ -25,6 +25,7 @@ export const TextInput = ({
 }: TextInputCoreProps): JSX.Element => {
 
   const classNames: string = React.useMemo(() => getInputClassNames(invalid, readOnly, disabled), [invalid, readOnly, disabled]);
+  const showInvalidIcon = React.useMemo(() => (!disabled && invalid), [touched, invalid]);
 
   return (
     <div className="relative rounded-md">
@@ -36,7 +37,7 @@ export const TextInput = ({
         readOnly={readOnly}
         {...htmlInputProps}
       />
-      {invalid && (
+      {showInvalidIcon && (
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
           <ExclamationCircleIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
         </div>
@@ -58,7 +59,7 @@ const styles = {
   baseStyles: 'block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
   default: 'text-gray-900 shadow-sm ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600',
   invalid: 'text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500',
-  readOnly: 'bg-gray-50 text-gray-500 ring-gray-300 cursor-not-allowed',
-  disabled: 'bg-gray-50 text-gray-500 ring-gray-300 cursor-not-allowed cursor-not-allowed opacity-50'
+  readOnly: 'bg-indigo-50 text-gray-600 placeholder:text-gray-500 placeholder:font-normal ring-gray-300 cursor-not-allowed tracking-wide',
+  disabled: 'bg-gray-50 text-gray-500 placeholder:text-gray-400 ring-gray-300 cursor-not-allowed cursor-not-allowed opacity-50'
 }
 
--- src/stories/TextInputField.stories.tsx
+++ src/stories/TextInputField.stories.tsx
@@ -7,7 +7,7 @@ export default {
   component: TextInputField,
   args: {
     type: 'text',
-    label: 'Input Label',
+    label: 'Favorite Book',
     value: '',
     invalid: false,
     required: false,
@@ -15,17 +15,19 @@ export default {
     disabled: false,
     hideLabel: false,
     inlineLabel: false,
-    placeholder: 'Enter your text...',
+    placeholder: 'e.g., the Cat in the Hat',
   },
 } as ComponentMeta<typeof TextInputField>;
 
-const validText = 'taco cat';
+const validText = 'tacocat';
 const errorText = `Value must be "${validText}".`;
 const clearErrorText = (errText: string, val:string): boolean => (errText === errorText && val === validText);
 
 const Template: ComponentStory<typeof TextInputField> = ({ value, invalid, ...args }: TextInputFieldProps) => {
   const [val, setVal] = React.useState<string>(value ?? '');
   const [touched, setTouched] = React.useState<boolean>(args.touched ?? false);
+
+  const containerWidth = args.inlineLabel ? 'w-96' : 'w-72';
   
   const shouldClearInvalid = React.useMemo(() => (
     clearErrorText(args.errorText || '', val)
@@ -41,7 +43,7 @@ const Template: ComponentStory<typeof TextInputField> = ({ value, invalid, ...ar
   }, [value]);
 
   return (
-    <div className='w-72'>
+    <div className={containerWidth}>
       <TextInputField
         {...args}
         invalid={isInvalid}
@@ -55,20 +57,30 @@ const Template: ComponentStory<typeof TextInputField> = ({ value, invalid, ...ar
 };
 
 export const DefaultTextInputField = Template.bind({});
-
+export const DisabledTextInputField = Template.bind({});
 export const InvalidTextInputField = Template.bind({});
+export const ReadOnlyTextInputField = Template.bind({});
+export const RequiredTextInputField = Template.bind({});
 
 InvalidTextInputField.args = {
   errorText,
+  label: 'Favorite Palindrome',
   invalid: true,
   touched: true,
-  value: 'Not taco cat',
+  placeholder: 'e.g., toot',
+  value: 'kayak',
 };
 
-export const RequiredTextInputField = Template.bind({});
+DisabledTextInputField.args = {
+  disabled: true,
+};
+
+ReadOnlyTextInputField.args = {
+  readOnly: true,
+  value: 'Leaves of Grass',
+};
 
 RequiredTextInputField.args = {
   required: true,
   errorText: 'This field cannot be blank.',
 };
-
